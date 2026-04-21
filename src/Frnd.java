3b5dbf273582010041f00fe9e1c8130515de2b68
Mayurjoshi00 <joshimayur@outlook.in>
2026-04-21 14:09:41 +0530

    frnd

---
+++ src/Frnd.java
@@ -0,0 +1,14 @@
+class f1 {
+    void display()
+    {
+        System.out.println("hello there");
+    }
+}
+public class Frnd
+{
+    public static void main(String[] args)
+    {
+        f1 fr=new f1();
+        fr.display();
+    }
+}
